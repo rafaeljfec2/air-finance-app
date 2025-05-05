e3409c8758e205d602d7a175ebc1732278bfc029
rafaeljfec2 <rafael.ferreira2@thomsonreuters.com>
2025-05-04 23:08:49 -0300

    fix

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Menu, Transition } from '@headlessui/react';
 import {
